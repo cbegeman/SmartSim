651b223c3f37642aa307b78a80b90b39596009ad
Carolyn Begeman <cbegeman@lanl.gov>
2022-04-18 16:43:22 -0500

    Update e3sm scripts

--- tutorials/badger/launch_db_for_e3sm_new_alloc.py
+++ tutorials/badger/launch_db_for_e3sm_new_alloc.py
@@ -1,8 +1,8 @@
 from glob import glob
 from smartsim import Experiment
 from smartsim.settings import SbatchSettings, RunSettings
-from smartsim.database import SlurmOrchestrator
-from smartsim.utils.log import get_logger, log_to_file
+from smartsim.database import Orchestrator
+from smartsim.log import get_logger, log_to_file
 from smartsim import slurm
 import sys
 import os
@@ -28,16 +28,19 @@ os.chdir(case_dir)
 if os.path.exists('{}/db_debug.log'.format(case_dir)):
     os.system('rm {}/db_debug.log'.format(case_dir))
 log_to_file('{}/db_debug.log'.format(case_dir))
-logger = get_logger()
+logger = get_logger('db_launcher')
 
 if DB_NODES > 0:
     exp = Experiment("db", launcher="slurm")
-    db = SlurmOrchestrator(port=db_port,
-                           db_nodes=DB_NODES, 
-                           batch=True, # launching as a seperate batch workload
-                           time="00:30:00",
-                           interface="ib0", # high speed network to use (usually ipogif0 for aries CrayXC 50)
-                          )
+    db = Orchestrator(launcher="slurm",
+                      port=db_port,
+                      db_nodes=DB_NODES, 
+                      account="condo",
+                      queue="acme-small",
+                      batch=True, # launching as a seperate batch workload
+                      time="00:30:00",
+                      interface="ib0", # high speed network to use (usually ipogif0 for aries CrayXC 50)
+                     )
     exp.start(db)
     os.environ['SSDB'] = f"{db.hosts[0]}:{db_port}"
     logger.debug('SSDB={}'.format(os.environ.get('SSDB')))
--- tutorials/badger/launch_db_in_e3sm_alloc.py
+++ tutorials/badger/launch_db_in_e3sm_alloc.py
@@ -2,7 +2,7 @@ from glob import glob
 from smartsim import Experiment
 from smartsim.settings import SbatchSettings, RunSettings
 from smartsim.database import Orchestrator
-from smartsim.utils.log import get_logger, log_to_file
+from smartsim.log import get_logger, log_to_file
 from smartsim import slurm
 import sys
 import os
@@ -29,7 +29,7 @@ os.chdir(case_dir)
 if os.path.exists('{}/db_debug.log'.format(case_dir)):
     os.system('rm {}/db_debug.log'.format(case_dir))
 log_to_file('{}/db_debug.log'.format(case_dir))
-logger = get_logger()
+logger = get_logger('db_launcher')
 #logger.debug('E3SM JOBID={}'.format(E3SM_JOBID))
 #logger.debug('DB NODES={}'.format(DB_NODES))
 
@@ -37,11 +37,11 @@ if DB_NODES > 0:
     exp = Experiment("db", launcher="local")
     db = Orchestrator(launcher="slurm",
                       port=db_port,
-                           db_nodes=DB_NODES, # only 1 database (not a cluster)
-                           batch=False, # not launching as a seperate batch workload
-                           interface="ib0", # high speed network to use (usually ipogif0 for aries CrayXC 50)
-                           alloc=E3SM_JOBID# launching into this allocation
-                          )
+                      db_nodes=DB_NODES, # only 1 database (not a cluster)
+                      batch=False, # not launching as a seperate batch workload
+                      interface="ib0", # high speed network to use (usually ipogif0 for aries CrayXC 50)
+                      alloc=E3SM_JOBID# launching into this allocation
+                     )
     exp.start(db)
     os.environ['SSDB'] = f"{db.hosts[0]}:{db_port}"
     logger.debug('SSDB={}'.format(os.environ.get('SSDB')))
