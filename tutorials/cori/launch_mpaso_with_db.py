1b8d60df6630a71aa0330c9ed595716ceaa0f6ba
Camille Renaud <camille.renaud@colorado.edu>
2022-07-27 14:37:44 -0700

    launch mpaso and dbnode with driver function

--- tutorials/cori/launch_mpaso_with_db.py
+++ tutorials/cori/launch_mpaso_with_db.py
@@ -2,36 +2,30 @@ from smartredis import Client
 from smartsim import Experiment
 from smartsim.database import Orchestrator
 
-MPASO_DIR="/global/homes/c/cbegeman/MPAS-Ocean-test-case-output/smartsim/ocean/baroclinic_channel/10km/default"
+MPASO_DIR="/global/cscratch1/sd/crenaud/e3sm_scratch/testsmartsim/ocean/soma/32km/long"
 
 print('Create Experiment object')
-exp = Experiment("mpas-ocean_simulation", launcher="slurm")
+exp = Experiment("mpas-ocean_simulation", launcher="local")
 print('Create database')
 db = exp.create_database(db_nodes=1,
                          port=6379,
-                         batch=True,
+                         #batch=True,
                          interface="lo",
                          time="00:30:00",
                          account="e3sm",
                          partition="debug",
                          batch_args={"C":"haswell"})
 
-
 # define how simulation should be executed
 print('Set run settings')
-settings = exp.create_run_settings(f"{MPASO_DIR}/forward/ocean_model", 
-                                   exe_args=["-n namelist.ocean",
-                                             "-s streams.ocean"])
-# Option 1 will look something like
-#settings = exp.create_run_settings(f"{MPASO_DIR}/run_ocean_model.sh")
-# And option 1's bash script will look something like
-#--------------------------
-#!/bin/bash
-#source load_compass_env.sh
-#compass run
-#--------------------------
+#settings = exp.create_run_settings(f"{MPASO_DIR}/forward/ocean_model", 
+#                                   exe_args=["-n namelist.ocean",
+#                                             "-s streams.ocean"])
 
-# Option 2 will look something like
+# Option 1 will look something like
+settings = exp.create_run_settings("bash", f"{MPASO_DIR}/run_ocean_model.sh")
+
+#Option 2 will look something like
 #settings = exp.create_run_settings("sbatch",
 #                                    f"{MPASO_DIR}/submit_ocean_model")
 
@@ -50,13 +44,16 @@ model = exp.create_model("mpas-ocean", settings)
 # start the database and connect client to get data
 print('Start database')
 exp.start(db)
+
+print(exp.summary())
+
 print('Create client object')
 client = Client(address="127.0.0.1:6379", cluster=False)
 
 # block=True appears to be used to make the model wait for the
 # client to be launched before execution
-#print('Start experiment')
-#exp.start(model, block=True, summary=True)
+print('Start experiment')
+exp.start(model, block=True, summary=True)
 
 # launch models, anaylsis, training, inference sessions, etc
 # that communicate with the database using the SmartRedis clients
