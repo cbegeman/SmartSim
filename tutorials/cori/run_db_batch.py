9e9417903a093b2604ffdc640447f661e35b0649
Carolyn Begeman <cmbranecky@gmail.com>
2022-07-27 08:15:57 -0700

    Update run_db_batch SSDB

--- tutorials/cori/run_db_batch.py
+++ tutorials/cori/run_db_batch.py
@@ -1,11 +1,14 @@
 from smartsim import Experiment
+import time
+import os
 
+db_port=6379
 exp = Experiment("batch-db-on-slurm", launcher="slurm")
 db_cluster = exp.create_database(db_nodes=1,
-                                 db_port=6780,
+                                 db_port=db_port,
                                  batch=True,
-                                 time="00:10:00",
                                  interface="lo",
+                                 time="00:30:00",
                                  account="e3sm",
                                  partition="debug",
                                  batch_args={"C":"haswell"})
@@ -15,5 +18,8 @@ exp.start(db_cluster)
 print(f"Orchestrator launched on nodes: {db_cluster.hosts}")
 # launch models, analysis, training, inference sessions, etc
 # that communicate with the database using the SmartRedis clients
-
-exp.stop(db_cluster)
+os.environ['SSDB'] = f"{db_cluster.hosts[0]}:{db_port}"
+print(os.environ.get('SSDB'))
+time.sleep(3600)
+#exp.stop(db_cluster)
+#SUCCESS
